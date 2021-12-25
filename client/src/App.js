fc026cecbfa6958bbd7cc955bb6c2cde8029e961
Eric Harvey <swimleft@gmail.com>
2021-12-24 19:48:59 -0800

    test

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h3>I did this!!!</h3>
       </header>
     </div>
   );
